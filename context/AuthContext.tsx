50f885aa59c44444b09572e9eb762e9edd243fc7
Frank Laércio <franklaercio@gmail.com>
2021-09-15 23:54:50 -0300

    Adjusting code

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -4,25 +4,25 @@ import { api } from "../services/api";
 type SignInCredentials = {
   email: string;
   password: string;
-}
+};
 
 type AuthContextData = {
   signIn(credentials): Promise<void>;
   isAuthenticated: boolean;
-}
+};
 
 type AuthProviderProps = {
   children: ReactNode;
-}
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    try { 
-      const response = await api.post('sessions', {
+    try {
+      const response = await api.post("sessions", {
         email,
         password,
       });
@@ -34,8 +34,8 @@ export function AuthProvider({children}: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{signIn, isAuthenticated}}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
       {children}
-    </AuthContext.Provider>  
+    </AuthContext.Provider>
   );
 }
