69f42edd6bb620828244f61ef937cb5b5d789bad
Frank Laércio <franklaercio@gmail.com>
2021-09-19 01:13:26 -0300

    Adding validate auth

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,25 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRAuth<P>(fun: GetServerSideProps<P>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["nextxauth.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fun(ctx);
+  };
+}
